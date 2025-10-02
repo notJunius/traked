1723804a96fb16d849ee1dfc660be02ff3a90e92
notJunius <120129256+notJunius@users.noreply.github.com>
2025-10-02 14:13:09 -0600

    got help from emzy, made a flake for raylib

--- flake.nix
+++ flake.nix
@@ -8,22 +8,28 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in {
-      devShells.x86_64-linux.default = pkgs.mkShell {
+      devShells.x86_64-linux.default = pkgs.mkShell rec {
         name ="Odin";
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath packages;
         nativeBuildInputs = with pkgs; [
           git
           which
           clang_20
           llvmPackages_20.llvm
           llvmPackages_20.bintools
+          pkg-config
         ];
         packages = with pkgs; [
           libGL
           xorg.libX11
+          xorg.libX11.dev
           SDL2
           SDL
           SDL2_mixer
           glfw
+          xorg.libXinerama
+          xorg.libXi
+          xorg.libXft
         ];
         shellHook = ''
           echo "You're in a devshell"
